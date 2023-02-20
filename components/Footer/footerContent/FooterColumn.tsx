99905f5c10313e5e10de1fd588c42d6f3ebeb5a3
loarsaw <amanahmed1385@gmail.com>
2023-02-20 09:34:31 +0530

    deploykey

--- components/Footer/footerContent/FooterColumn.tsx
+++ components/Footer/footerContent/FooterColumn.tsx
@@ -11,18 +11,19 @@ const FooterColumn = (props: Props) => {
     <div className="flex justify-between flex-wrap flex-grow min-width-[800px] xl:rtl:pl-60">
       {footerContent.map((item) => {
         return (
-          <div className="mt-6 md:mt-0">
+          <div className="mt-6 md:mt-0" key={item.title}>
             <h2 className="text-md rtl:border-r-4 ltr:border-l-4 border-palette-primary px-2">
               {t[item.title]}
             </h2>
             <div className="flex flex-col mt-2">
               {item.subtitles.map((subItem) => {
                 return (
-                  <a
+                  <div
+                  key={subItem.text}
                   className="text-sm text-palette-base/90 px-4 py-2 hover:text-palette-base/100"
                   >
                     {t[subItem.text]}
-                  </a>
+                  </div>
                 );
               })}
             </div>
